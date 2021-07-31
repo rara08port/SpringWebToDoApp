e7f004cb9a9fc8770481f498cdfbccfd9db35f33
rara08port <k.kblsh4058@gmail.com>
2021-07-31 18:58:23 +0900

    modified SignUpController commit

--- src/main/java/com/example/demo/controller/SignUpController.java
+++ src/main/java/com/example/demo/controller/SignUpController.java
@@ -36,27 +36,22 @@ public class SignUpController {
      * @return resources/templates下のHTMLファイル名
      */
     @GetMapping("/signup")
-    public String getSignUp(SignUpForm signupForm,Model model) {
-    	System.out.println("getSignUp");
-
-        //Taskのリストを取得する
-        //List<Task> taskList = taskService.findAll();
-
-        //model.addAttribute("taskList", taskList);
-    	model.addAttribute("signupForm", signupForm);
+    public String getSignUp(SignUpForm signUpForm,Model model) {
+    	
+    	model.addAttribute("signUpForm", signUpForm);
         model.addAttribute("title", "ユーザ登録");
 
         return "user/signup";
     }
     
     @PostMapping("/signup")
-    public String postSignUp(@Validated @ModelAttribute SignUpForm signupForm,
+    public String postSignUp(@Validated @ModelAttribute SignUpForm signUpForm,
     		BindingResult result,
     		Model model) {
     	
     	User user = new User();
-    	user.setUsername(signupForm.getUsername());
-    	user.setPassword(signupForm.getPassword());
+    	user.setUsername(signUpForm.getUsername());
+    	user.setPassword(signUpForm.getPassword());
     	user.setAdmin_flg(0);
     	int count=10;
     	count = userService.findRegisterUser(user);
@@ -74,12 +69,7 @@ public class SignUpController {
     	//return "redirect:/task/index";
     	 return "user/signup";
     	 */
-    	System.out.println(result.hasErrors());
-    	System.out.println(result.getFieldError("username"));
-    	System.out.println(result.getFieldError("password"));
-    	//fields.hasErrors('username');
-    	System.out.println(result.getFieldErrorCount());
-    	System.out.println(result.getFieldErrors("username"));
+    	
     	
     	if (!result.hasErrors()) {  	
     		System.out.println("ユーザ登録");
@@ -87,37 +77,13 @@ public class SignUpController {
     		//return "task/index";
     		return "redirect:/task";
         } else {
-        	System.out.println("項目エラー");
-        	model.addAttribute("signupForm", signupForm);
-        	System.out.println(signupForm.getUsername());
-        	System.out.println(signupForm.getPassword());
+        	
+        	model.addAttribute("signuUpForm", signUpForm);
         	model.addAttribute("title", "ユーザ登録再");
         	
         	 return "user/signup";
             
         }
-    	/*
-    	if (!result.hasErrors()) {
-    		Task task = new Task();
-    		task.setId(taskId);
-        	//task.setUserId(1);
-        	task.setTypeId(taskForm.getTypeId());
-        	task.setTitle(taskForm.getTitle());
-        	task.setDetail(taskForm.getDetail());
-        	task.setDeadline(taskForm.getDeadline());
-
-        	taskService.update(task);
-        	
-        	redirectAttributes.addFlashAttribute("complete", "変更が完了しました");
-
-        	return "redirect:/task/" + taskId;
-        } else {
-            model.addAttribute("taskForm", taskForm);
-            model.addAttribute("title", "タスク一覧");
-            return "task/index";
-            
-        }
-        */
     	
     }
     
